f820c5e791893ed8a600a0a3208aead3a9781de3
ueux <ueux-github@mikey.codes>
2021-10-19 18:51:35 +0100

    adds the ability to add, remove and find a node on the consistent ring

--- conhash.go
+++ conhash.go
@@ -1,21 +1,74 @@
 package conhash
 
 import (
+	"errors"
 	"fmt"
 	"hash/fnv"
 	"sort"
 )
 
+// Errors relating to common ring issues when handling nodes.
+var (
+    ErrAddExceedsRingSize = errors.New("cannot add a new node, new size will exceed maximum ring size")
+    ErrNodeNotFound = errors.New("node not found")
+)
 // Ring represents a circular ring that stores the provided nodes.
-// The ring size is set to len(nodes) << 2.
+// The ring size is set to len(nodes) << 3.
 type Ring struct{
     Size uint64
     Nodes []Node
 }
 
-// Position finds the location where a key is placed on a ring.
-func (r Ring) Position(key string) uint64 {
-    return hash(key) % r.Size
+// FindByHash searches through the ring to find the closest node for a given hash.
+func (r Ring) Find(key string) Node {
+    hashID := hash(key) % r.Size
+    i := sort.Search(len(r.Nodes), func(i int) bool {
+        return r.Nodes[i].hashID >= hashID
+    })
+
+    if i >= len(r.Nodes) {
+        i = 0
+    }
+
+    return r.Nodes[i]
+}
+
+// Add will add a new node into the ring, if the new size would exceed the ring size
+// the function will return an error rather than increase the size creating a full 
+// rehash requirement on all nodes.
+func (r *Ring) Add(n Node) error {
+    if uint64(len(r.Nodes)+1) > r.Size {
+        return ErrAddExceedsRingSize
+    }
+
+    n.hashID = hash(fmt.Sprintf("%s:%d", n.Host, n.Port)) % r.Size
+    r.Nodes = append(r.Nodes, n)
+
+    sort.Slice(r.Nodes, func(i, j int) bool {
+        return r.Nodes[i].hashID < r.Nodes[j].hashID
+    })
+
+    return nil
+}
+
+// Remove will remove an existing node from the ring, if the node cannot be found
+// then an error will be returned.
+func (r *Ring) Remove(node Node) error {
+    idx := -1
+    for i, n := range r.Nodes {
+        if n.Host == node.Host && n.Port == node.Port {
+            idx = i
+        }
+    }
+
+    if idx == -1 {
+        return ErrNodeNotFound
+    }
+
+    r.Nodes[idx] = r.Nodes[len(r.Nodes)-1]
+    r.Nodes = r.Nodes[:len(r.Nodes)-1]
+
+    return nil
 }
 
 // Node represents a host machine.
@@ -23,23 +76,23 @@ type Node struct {
     Host string
     Port int
 
-    position uint64
+    hashID uint64
 }
 
 // New will take in a slice of nodes and placed them in ascending order
 // on the network ring.
 func New(nodes []Node) Ring {
     r := Ring{
-        Size: uint64(len(nodes) << 2),
+        Size: uint64(len(nodes) << 3),
     }
 
     for _, n := range nodes {
-        n.position = r.Position(fmt.Sprintf("%s:%d", n.Host, n.Port))
+        n.hashID = hash(fmt.Sprintf("%s:%d", n.Host, n.Port)) % r.Size
         r.Nodes = append(r.Nodes, n)
     }
 
     sort.Slice(r.Nodes, func(i, j int) bool {
-        return r.Nodes[i].position < r.Nodes[j].position
+        return r.Nodes[i].hashID < r.Nodes[j].hashID
     })
 
     return r
--- conhash_test.go
+++ conhash_test.go
@@ -16,8 +16,22 @@ func TestNew(t *testing.T) {
             Name: "empty ring if no nodes provided",
             Nodes: nil,
             ExpectedRing: conhash.Ring{
-                Size: 0 << 2,
+                Size: 0 << 3,
                 Nodes: nil,
+            },
+        },
+        {
+            Name: "ring creates with provided nodes",
+            Nodes: []conhash.Node{
+                {Host: "localhost", Port: 1000},
+                {Host: "localhost", Port: 1001},
+            },
+            ExpectedRing: conhash.Ring{
+                Size: 2 << 3,
+                Nodes: []conhash.Node{
+                    {Host: "localhost", Port: 1000},
+                    {Host: "localhost", Port: 1001},
+                },
             },
         },
     }
@@ -33,5 +47,78 @@ func TestNew(t *testing.T) {
                 t.Fatalf("got node length[%d], want node length[%d]", len(r.Nodes), len(tc.ExpectedRing.Nodes))
             }
         })
+    }
+}
+
+func TestAdd(t *testing.T) {
+    nodes := []conhash.Node{
+        {Host: "localhost", Port: 1000},
+        {Host: "localhost", Port: 1001},
+    }
+    r := conhash.New(nodes)
+
+    node := conhash.Node{Host: "localhost", Port: 1002}
+    err := r.Add(node)
+    if err != nil {
+        t.Fatalf("got error[%v] but wanted error[nil]", err)
+    }
+
+    if len(r.Nodes) != 3 {
+        t.Fatalf("got length[%d], but want node length[%d]", len(r.Nodes), 3)
+    }
+
+    found := false
+    for _, n := range r.Nodes {
+        if n.Port != node.Port {
+            found = true
+            break
+        }
+    }
+
+    if !found {
+        t.Fatal("could not find expected added node")
+    }
+}
+
+func TestRemove(t *testing.T) {
+    nodes := []conhash.Node{
+        {Host: "localhost", Port: 1000},
+        {Host: "localhost", Port: 1001},
+    }
+    r := conhash.New(nodes)
+
+    node := conhash.Node{Host: "localhost", Port: 1001}
+    err := r.Remove(node)
+    if err != nil {
+        t.Fatalf("got error[%v] but wanted error[nil]", err)
+    }
+
+    if len(r.Nodes) != 1 {
+        t.Fatalf("got length[%d], but want node length[%d]", len(r.Nodes), 1)
+    }
+
+    exists := false
+    for _, n := range r.Nodes {
+        if n.Port == node.Port {
+            exists = true
+            break
+        }
+    }
+
+    if exists {
+        t.Fatal("found expected removed node")
+    }
+}
+
+func TestFind(t *testing.T) {
+    nodes := []conhash.Node{
+        {Host: "localhost", Port: 1000},
+        {Host: "localhost", Port: 1001},
+    }
+    r := conhash.New(nodes)
+
+    node := r.Find("foobar")
+    if node.Port != 1001 {
+        t.Fatalf("got node port[%d] but want node port[%d]", node.Port, 1001)
     }
 }
